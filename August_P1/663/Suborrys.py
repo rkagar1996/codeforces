91db32d7d2f84c8a85cd6c79d8794df12588950a
rkagar1996 <69521224+rkagar1996@users.noreply.github.com>
2020-08-11 20:28:02 +0530

    Create Suborrys.py

---
+++ August_P1/663/Suborrys.py
@@ -0,0 +1,11 @@
+! https://codeforces.com/contest/1391/problem/A
+
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    for j in range(1,n+1):
+        print(f"{j} ")
+        
+        
+        
